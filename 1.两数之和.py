f70c080226516859d2e62ddb2afc58baae8fc3e7
刘昌灵 <liuchangling@liuchanglingdeMacBook-Pro.local>
2019-11-04 10:44:28 +0800

    1 done

---
+++ 1.两数之和.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        m = {}
+        for index, value in enumerate(nums):
+            # 找到个合适的就返回
+            if value in m :
+                return [m[value], index]
+            # 没找时，将target-value 和index存入map
+            else :
+                m[target-value] = index
+
+# @lc code=end
+
