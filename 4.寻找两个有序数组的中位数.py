3e6bc7ae84451f2766f5682232c41875e0a798f9
lcl <444571321@qq.com>
2019-11-12 17:27:27 +0800

    4 看了一波答案  这个处理边界的思路太神了

--- 4.寻找两个有序数组的中位数.py
+++ 4.寻找两个有序数组的中位数.py
@@ -10,93 +10,53 @@
 #           -  L1 + L2的个数 和 R1 + R2个数相等。 满足这一点只需要偏移时，另一个数组等量反向移动即可
 #       移动方式, 哪多了就缩小，哪少了就扩大。
 #       二分查找步长直接从1,2中较短数组的长度不断右移一位， 直至为0
-
+# 方法2改进，为了避免考虑过多的边界情况，这里采用一个特殊技巧
+# 将1 m 视为虚拟2m+1
+# 将2 n 视为虚拟2n+1
+#  ～ 1 ～ 2 ～ 3 ～  => cut 2
+#  ～ 1 ～ 2 ～ 3 ～ 4 ～ => cut 2&3中间的～
 
 
 # @lc code=start
 from typing import List
 
+import sys
+maxN = sys.maxsize
+
+
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # 初始化直接用中位数搞起
-        c1 = (len(nums1) -1 ) >> 1 
-        c2 = (len(nums2) -1 ) >> 1 
-        obj1 = self.cutArray(nums1, c1)
-        obj2 = self.cutArray(nums2, c2)
-        k = min(len(nums1), len(nums2))
+        len1 = len(nums1)
+        len2 = len(nums2)
 
-        while obj1["notEnd"] and obj2["notEnd"] and (obj1["LVal"] > obj2["RVal"] or obj2["LVal"] > obj1["RVal"]):
-            k = k >> 2
-            if obj1["LVal"] > obj2["RVal"]:
-                # 说明数组1的左边多了点儿
-                # c1 左移k/2 c2 右移k/2
-                c1 = c1 - k 
-                c2 = c2 + k
-            elif obj2["LVal"] > obj1["RVal"]:
-                # 说明数组2的左边多了点儿
-                # c2 左移k/2 c1 右移k/2
-                c1 = c1 + k 
-                c2 = c2 - k
-
-            obj1 = self.cutArray(nums1, c1)
-            obj2 = self.cutArray(nums2, c2)
-
-        # print('done')
+        # 保证len1较小
+        if len1 > len2:
+            return self.findMedianSortedArrays(nums2, nums1)
         
-        mid = []
-        if len(nums1) == 0:
-            pass
-        elif len(nums1) % 2 == 1:
-            mid.append(nums1[obj1["LIndex"]])
-        else :
-            mid.append(nums1[obj1["LIndex"]])
-            mid.append(nums1[obj1["RIndex"]])
+        low = 0 
+        high = 2 * len1 # 神一般的思路，虚拟#解决奇偶坑爹边界问题
 
-        if len(nums2) == 0:
-            pass
-        elif len(nums2) % 2 == 1:
-            mid.append(nums2[obj2["LIndex"]])
-        else :
-            mid.append(nums2[obj2["LIndex"]])
-            mid.append(nums2[obj2["RIndex"]])
+        while low <= high:
+            c1 = (low + high) >> 1
+            c2 = len1 + len2 - c1
 
-        mid.sort()
-        print(mid)
+            lMax1 = nums1[(c1-1)>>1] if c1 != 0 else -maxN
+            rMin1 = nums1[c1>>1] if c1 != 2 * len1 else maxN
+
+            lMax2 = nums2[(c2-1)>>1] if c2 != 0 else -maxN
+            rMin2 = nums2[c2>>1] if c2 != 2 * len2 else maxN
+
+            if lMax1 > rMin2:
+                high = c1 - 1
+            elif lMax2 > rMin1:
+                low = c1 + 1
+            else:
+                break
+
+        return (max(lMax1, lMax2) + min(rMin1, rMin2))/2
+
         
-       
-        if len(mid) %2 == 1:
-            answer =  mid[len(mid) >> 1]
-        else:
-            i = (len(mid) -1) >> 1
-            answer = (mid[i] + mid[i+1]) /2
-        
-        print(answer)
-        return answer
 
-    
+# print(Solution().findMedianSortedArrays([2], [1, 3, 4, 5]))
 
-    def cutArray(self, array: List[int], location:int) :
-        print(array, location)
-        length = len(array)
-        
-        # 左切即为location
-        LIndex = location
-        LVal = None if LIndex - 1 < 0 else array[LIndex - 1]
-        # array长度为奇数时,RIndex 为location;偶数时 RIndex为location +1
-        RIndex = location if length % 2 == 1 else location + 1
-        RVal = None if RIndex + 1 >= length else array[RIndex + 1]
-        notEnd = not (LVal is None or RVal is None)
-
-        return {
-            "LIndex" : LIndex,
-            "RIndex" : RIndex,
-            "LVal" : LVal,
-            "RVal" : RVal,
-            "notEnd" : notEnd
-        }
-
-# todo fix 这种边界情况。。。
-Solution().findMedianSortedArrays([1], [2,3,4])
-        
 # @lc code=end
-
