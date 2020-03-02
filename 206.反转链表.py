4630cde9f40aeee26513a54a8dbab5de0d47f561
lcl <444571321@qq.com>
2020-03-02 13:09:05 +0800

    翻转链表

---
+++ 206.反转链表.py
@@ -0,0 +1,49 @@
+#
+# @lc app=leetcode.cn id=206 lang=python3
+#
+# [206] 反转链表
+#
+# https://leetcode-cn.com/problems/reverse-linked-list/description/
+#
+# algorithms
+# Easy (67.48%)
+# Likes:    791
+# Dislikes: 0
+# Total Accepted:    169.4K
+# Total Submissions: 250.9K
+# Testcase Example:  '[1,2,3,4,5]'
+#
+# 反转一个单链表。
+# 
+# 示例:
+# 
+# 输入: 1->2->3->4->5->NULL
+# 输出: 5->4->3->2->1->NULL
+# 
+# 进阶:
+# 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# 迭代法思路很简单，记录prev和head，并临时保存next
+# 一顿操作之后 prev和head右移一位即可。 速度94%
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        while head:
+            next = head.next
+            head.next = prev
+            prev = head
+            head = next
+        return prev
+
+# @lc code=end
